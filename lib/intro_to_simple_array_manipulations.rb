bf26304317646bd8b08bc2c9df58e5ecbf1b3019
Bleak Chandler <bleakchandler@gmail.com>
2021-01-19 20:20:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -64,8 +64,8 @@ end
 
 
 def using_delete_at(array, integer)
- famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-    
+     famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+     deleted_robot = using_delete_at(famous_robots, 2)
 
 end
 
